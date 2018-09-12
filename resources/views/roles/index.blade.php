d78abef25673cbc19d3fc1c51baaa99fd1d7d793
ecarrizo1978 <edison.carrizo.j@gmail.com>
2018-09-11 23:28:47 -0300

    estilo listar roles

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,63 +1,132 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Roles
-                    @can('roles.create')
-                        <a href="{{ route('roles.create') }}" class="btn btn-sm btn-primary pull-right">Crear</a>
-                    @endcan
-                </div>
-
-                <div class="panel-body">
-
-                    <table class="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                                <th width="10px" >ID</th>
-                                <th>Nombre</th>
-                                <th colspan="3">&nbsp;</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($roles as $c)
-                                <tr>
-                                    <td>{{ $c->id }}</td>
-                                    <td>{{ $c->name }}</td>
-                                    <td width="10px">
-                                        @can('roles.show')
-                                            <a href="{{ route('roles.show', $c->id) }}" class="btn btn-sm btn-default">
-                                                Ver
-                                            </a>
-                                        @endcan
-                                    </td>
-                                    <td width="10px">
-                                        @can('roles.edit')
-                                            <a href="{{ route('roles.edit', $c->id) }}" class="btn btn-sm btn-default">
-                                                Editar
-                                            </a>
-                                        @endcan
-                                    </td>
-                                    <td width="10px">
-                                        @can('roles.destroy')
-                                            {!!  Form::open(['route' => ['roles.destroy', $c->id], 'method' => 'DELETE' ])   !!}
-                                                <button class="btn btn-sm btn-danger">
-                                                    Eliminar
-                                                </button>
-                                            {!! Form::close()  !!}
-                                        @endcan
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{ $roles->render() }}
-                </div>
-            </div>
+@extends('admin.layout')
+@section('contenido')
+<div id="tabla" class="col-sm-12">
+    <div class="white-box">
+        <div class="panel panel-info">
+            <div class="panel-heading"><span class="mdi mdi-24px mdi-account-convert"></span> Administracion de Roles</div>
+        </div>
+        <div class="table-responsive">
+            <table id="listusers" class="display nowrap" cellspacing="0" width="100%">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    @foreach($roles as $c)
+                        <tr>
+                            <td>{{ $c->id }}</td>
+                            <td>{{ $c->name }}</td>
+                            <td>{{ $c->description }}</td>
+                            <td>
+                                @can('roles.show')
+                                    <a href="{{ route('roles.show', $c->id) }}" class="btn btn-success waves-effect waves-light" role="button">
+                                        <span class="btn-label">
+                                            <i class="fa fa-search"></i>
+                                        </span>Ver
+                                    </a>
+                                @endcan
+                            </td>
+                            <td>
+                                @can('roles.edit')
+                                    <a href="{{ route('roles.edit', $c->id) }}" class="btn btn-warning waves-effect waves-light" role="button">
+                                        <span class="btn-label">
+                                            <i class="fa fa-edit"></i>
+                                        </span>Editar
+                                    </a>
+                                @endcan
+                            </td>
+                            <td>
+                                @can('roles.destroy')
+                                    {!!  Form::open(['route' => ['roles.destroy', $c->id], 'method' => 'DELETE' ])   !!}
+                                        <button class="btn btn-danger waves-effect waves-light" type="button">
+                                            <span class="btn-label">
+                                                <i class="fa fa-trash"></i>
+                                            </span>Eliminar
+                                        </button>
+                                    {!! Form::close()  !!}
+                                @endcan
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
+<link href = "{{ URL::asset('plugins/bower_components/datatables/jquery.dataTables.min.css')   }}" rel="stylesheet" type="text/css"   />
+<link href = "{{ URL::asset('plugins/DataTables/Buttons-1.5.1/css/buttons.dataTables.min.css') }}" rel="stylesheet" type="text/css"   />
+<script  src="{{ URL::asset('plugins/DataTables/datatables.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/DataTables/DataTables-1.10.16/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/DataTables/Buttons-1.5.1/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/DataTables/Buttons-1.5.1/js/buttons.flash.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/DataTables/JSZip-2.5.0/jszip.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/DataTables/pdfmake-0.1.32/pdfmake.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/DataTables/pdfmake-0.1.32/vfs_fonts.js') }}"></script>
+<script src="{{ URL::asset('plugins/DataTables/Buttons-1.5.1/js/buttons.html5.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/DataTables/Buttons-1.5.1/js/buttons.print.min.js') }}"></script>
+<!-- end - This is for export functionality only -->
+<script>
+$('#listusers').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+        'copy', 'csv', 'excel', 'pdf', 'print',
+        {
+            text: 'Crear Rol',
+            action: function ( e, dt, node, config ) 
+            {
+                 window.location.href = '{{ route('roles.create') }}';
+            }
+        }
+    ],
+    columnDefs: [{
+            "targets": [3, 4, 5],
+            "orderable": false
+        }],
+    language: {
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "buttons": {
+            "copy": 'Copiar',
+            "csv": 'Exportar a CSV',
+            "print": 'Imprimir'},
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+    }
+});
+</script>
+
 @endsection
